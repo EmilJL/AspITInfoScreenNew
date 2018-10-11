164ede3edc6c39ace00c055b88852c6844f786f4
Daniel Nicolas Birk Holm <dani479d@edu.campusvejle.dk>
2018-10-11 13:55:00 +0200

    Changed weather image to Bitmap. Added Date field.

--- AspITInfoScreen/MainPage.xaml.cs
+++ AspITInfoScreen/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Diagnostics;
+using System.Net;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,26 +25,46 @@ namespace AspITInfoScreen
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        BitmapImage bmap = new BitmapImage();
         public MainPage()
         {
             this.InitializeComponent();
             SetWeatherImage();
+            TBlockDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
         }
         /// <summary>
-        /// 
+        /// Opens a weather chart from DMI in a WebView.
         /// </summary>
         public void SetWeatherImage()
         {
             try
             {
                 Uri address = new Uri("http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by=2630&mode=long");
-                WebWeather.Source = address;
+
+                bmap.DecodePixelType = DecodePixelType.Logical;
+                bmap.DecodePixelWidth = (int)MyGrid.ColumnDefinitions.Select(c => c.ActualWidth).FirstOrDefault();
+                bmap.DecodePixelHeight = (int)MyGrid.RowDefinitions.Select(c => c.ActualHeight).FirstOrDefault();
+                bmap.UriSource = address;
+
+                ImageWeather.Source = bmap;
+
             }
             catch (Exception error)
             {
-                Debug.WriteLine(error.Message);
+                Debug.WriteLine(error.GetType() + ": " + error.Message);
             }
             
+        }
+
+        public void SetComicStripImage()
+        {
+
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            bmap.DecodePixelWidth = (int)MyGrid.ColumnDefinitions.Select(c => c.ActualWidth).FirstOrDefault();
+            bmap.DecodePixelHeight = (int)MyGrid.RowDefinitions.Select(c => c.ActualHeight).FirstOrDefault();
         }
     }
 }
